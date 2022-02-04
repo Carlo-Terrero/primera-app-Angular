b3c6a5dfc0f17dbaa5e8aeb115425c06ea641f66
Carlo-Terrero <carlosjose2111@gmail.com>
2022-02-04 14:00:07 +0000

    agregamos servicio a peliculas

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -1,11 +1,13 @@
 // SIEMPRE que se utilcen hooks, tienes que importalos e implementarlos en implements
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 import { Pelicula } from '../../models/pelicula';
+import { PeliculaService } from '../../services/pelicula.service'
 
 @Component({
   selector: 'app-peliculas', /* el selector es editable aunque el componente se genere por comandos */
   templateUrl: './peliculas.component.html',
-  styleUrls: ['./peliculas.component.css']
+  styleUrls: ['./peliculas.component.css'],
+  providers: [PeliculaService]
 })
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
@@ -20,40 +22,40 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   /* A pesar de que se parecn mucho estos dos metodos en el constructo no se le debe meter logica dentro, 
   eso seria dentro del OnInit. En el constructor sole se le da valor a las propiedades de la clase  y
   precargar cosas necesarias */
-  constructor() {
+  constructor(
+    private _peliculaService: PeliculaService
+  ){
     this.pelifab = '';
     this.titulo = "Componente peliculas";
-    this.peliculas = [
+    this.peliculas = /* [
       new Pelicula("Spider-Man: No Way Home", 2021, "https://i.blogs.es/cb2ce6/spiderman-no-way-home-cartel/1366_2000.jpeg"),
       new Pelicula("Venom", 2018, "https://depor.com/resizer/vod5Glu5S--OQLDKvOcYnmISghQ=/1200x675/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/Y653K6IYRJGCBKBSYXR36COJD4.jpg"),
       new Pelicula("Guardianes de la Galaxia", 2018, "https://as01.epimg.net/meristation/imagenes/2021/10/12/noticias/1634031467_926414_1634031548_noticia_normal.jpg"),
       {year:2022, title: "Spiderman 4", image: "https://e00-marca.uecdn.es/assets/multimedia/imagenes/2021/12/18/16398555270572.jpg"},
       {year:2020, title: "Vangadores endgame", image: "https://www.cinemascomics.com/wp-content/uploads/2021/09/vengadores-endgame-marvel-studios.jpg"},
       {year:2018, title: "Batman vs Superman", image: "https://i.ytimg.com/vi/eyATkmsSZJk/sddefault.jpg"},
-      {year:2019, title: "Thor Ragnarok", image: "https://hipertextual.com/wp-content/uploads/2017/10/thor_ragnarok_taika_waititi.jpg"}
-    ]
-    this.fecha = new Date(2022, 2, 3)
+      {year:2019, title: "Thor Ragnarok", image: "https://hipertextual.com/wp-content/uploads/2017/10/thor_ragnarok_taika_waititi.jpg"}      
+    ]  */
+    this._peliculaService.getPeliculas(); //Esta es la forma mas optima de tener la peliculas. Obteniendolas a traves del servicio.
+    this.fecha = new Date(2022, 2, 3);
   }
-
 
   /* Este Methodo se ejecuta cuando se utiliza este componente(cuando es llamado) 
   y es cargado despues del construtor y luego el resto */
   ngOnInit(): void {
     console.log("componente on init inicializado");
     console.log(this.peliculas);
+    console.log(this._peliculaService.holaMundo())
   }
-
 
   //Este methodo se ejecuta cuando se hace algun cambio en el componente o la app (constantemente)
   ngDoCheck(){
     console.log("El methodo DoCheck lanzado")
   }
 
-
   cambiarTitulo(){
     this.titulo = "El titulo ha sido cambiado";
   }
-
 
   //Este methodo nos avisa de que se va a eliminar el componente. Justo antes de eliminar la instancia de componente se ejecuta
   ngOnDestroy(){
---
+++ src/app/services/pelicula.service.ts
@@ -0,0 +1,30 @@
+/* 
+1º Importamos el objeto Injectable que permite aplicar un decorador a la clase y cuando necesite usar el servicio no 
+tengo que craar el objeto como tal. 
+2º Luego importamos el modelo. */
+
+import { Injectable } from '@angular/core';
+import { Pelicula } from '../models/pelicula';
+
+//Definimos el servicio. Con el servicion se llevamos los datos al componente donde se necesiten.
+@Injectable()
+export class PeliculaService{
+
+    public peliculas: Pelicula[]
+
+    constructor(){
+        this.peliculas = [
+            new Pelicula("Sing 2", 2021, "https://m.media-amazon.com/images/I/71cYOjYT-LL._SY355_.jpg"),
+            new Pelicula("Lunáticos", 2015, "https://www.bizcochito.es/Films/Movil/tt14534992.jpg"),
+            new Pelicula("The King's Man", 2019, "https://www.jotdown.es/wp-content/uploads/2021/12/Portada-2.jpg"),
+        ]
+    }
+
+    holaMundo(){
+        return 'Hola Mundo desde el servicio pelicula'
+    }
+
+    getPeliculas(){
+        return this.peliculas;
+    }
+}
