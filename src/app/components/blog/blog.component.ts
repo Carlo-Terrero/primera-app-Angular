8cc4e669e9fdf264cafa3f6b101a4a138f94be6b
Carlo-Terrero <carlosjose2111@gmail.com>
2022-02-04 19:17:30 +0000

    mostramos listado de objetos del bbdd en frontend

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit,  } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
+import { Article } from '../../models/article';
+import { Global } from '../../services/global'
 
 @Component({
   selector: 'app-blog',
@@ -9,17 +11,24 @@ import { ArticleService } from '../../services/article.service';
 })
 export class BlogComponent implements OnInit {
 
+  public articles: Article[] = [];
+  public url: string;
+
   constructor(
-    private _ArticleService: ArticleService
-  ) {
-    
+    private _ArticleService: ArticleService,
+  ){
+    this.url = Global.url;
   }
 
   ngOnInit(): void {
     // Con el methodo subscribe del observable nos permite recoger los datos que nos devuelve la petición
     this._ArticleService.getArticles().subscribe(
-      response => {
-        console.log(response);
+      response => {        
+        if(response.articles){          
+          this.articles = response.articles;
+        }else{
+
+        }
       },
       error => {
         console.log(error)
